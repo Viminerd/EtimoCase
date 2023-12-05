b4f2393c2e32ca2aefb1c47d7c61e8e9cf89f892
Viktor Mineur <vikmi721@student.liu.se>
2023-12-05 11:18:38 +0100

    added reset company function

--- containers/company.py
+++ containers/company.py
@@ -5,15 +5,21 @@ class company_class:
         self.size = 0
         
     def add(self,employee):
-        employee.email = employee.email.lower()
-        self.employees[employee.email] = employee
-        self.size += 1
+        if employee.email not in self.employees:
+            employee.email = employee.email.lower()
+            self.employees[employee.email] = employee
+            self.size += 1
+            return True
+        else:
+            return False
     
     def delete(self, employee):
         if employee.email in self.employees:
             removed_employee = self.employees.pop(employee.email)
             self.size -= 1
-            return removed_employee
+            return True
+        else:
+            return False
         
     def get_employees(self):
         return self.employees
@@ -25,6 +31,13 @@ class company_class:
         if email in self.employees:
             return True       
         return False 
+    
+    def reset(self, reset_passsword):
+        if (reset_passsword == "Admin_password"):
+            self.employees.clear()
+            self.size=0
+            return True
+        return False
                     
     
 
